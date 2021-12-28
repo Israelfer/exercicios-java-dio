fae1c9260b03afc3de70508456ad309e5cd760e6
Israel Batista <israel.fbB@gmail.com>
2021-12-28 10:06:08 -0300

    Ex1_OrdemInversa feito

---
+++ src/arrays/Ex1_OrdemInversa.java
@@ -0,0 +1,25 @@
+package arrays;
+
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        int[] vetor = {-7, 0, 1, 7, 11, 17};
+
+        System.out.print("Vetor: ");
+        int count =0;
+        while(count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nVetor: ");
+        for(int i = (vetor.length - 1); i >= 0; i --) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
+
